b1500dabd9c9f483271d13579dbe8aad007c68c7
iliyazz <iliyaz.softuni@gmail.com>
2022-11-15 00:18:22 +0200

    Create Exercise Client Side Rendering
    01.List-Towns and 02.HTTP-Status-Cats

---
+++ 14-Exercise Client Side Rendering/02.HTTP-Status-Cats/app.js
@@ -0,0 +1,45 @@
+import { html, render } from './node_modules/lit-html/lit-html.js';
+import { cats } from './catSeeder.js';
+
+const root = document.getElementById('allCats');
+
+const catTemplate = html`
+    <ul>
+        ${cats.map(x => createCatCard(x))}
+    </ul>;
+`;
+
+render(catTemplate, root);
+
+function createCatCard(cat) {
+    return html`
+        <li>
+            <img src="./images/${cat.imageLocation}.jpg" width="250" height="250" alt="Card image cap">
+            <div class="info">
+                <button @click=${showContent} class="showBtn">Show status code</button>
+                <div class="status" style="display: none" id="${cat.id}">
+                    <h4>Status Code: ${cat.statusCode}</h4>
+                    <p>${cat.statusMessage}</p>
+                </div>
+            </div>
+        </li>
+    `
+}
+
+function showContent(e) {
+    const container = e.target.parentElement.querySelector('div');
+    const currentState = container.style.display;
+    if (currentState === 'none') {
+        container.style.display = 'block';
+        e.target.textContent = "Hide status code";
+    }
+    else {
+        container.style.display = 'none';
+        e.target.textContent = "Show status code";
+    }
+}
+
+
+
+
+
